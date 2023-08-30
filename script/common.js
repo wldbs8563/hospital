b9b426a349cbbc0a6480f2c8a1bd8a62fe2e70c2
jiyun <wldbs8563@gmail.com>
2023-08-30 18:20:57 +0900

    0830

--- script/common.js
+++ script/common.js
@@ -5,6 +5,7 @@ const nav_line = document.querySelectorAll('nav ul li > a')
 console.log(nav_bg, sub, nav)
 nav_bg.style.display='none'
 for(let i of sub){i.style.display='none'}
+for(let i of sub){i.style.borderTop='3px solid #fff'}
 nav.forEach(function(t,i){
     t.addEventListener('mouseover',function(){
         for(let i of sub){i.style.display='flex'}
@@ -15,7 +16,7 @@ nav.forEach(function(t,i){
     t.addEventListener('mouseout',function(){
         for(let i of sub){i.style.display='none'}
         nav_bg.style.display='none'
-        sub[i].style.borderTop='none'
+        sub[i].style.borderTop='3px solid #fff'
         sub[i].style.background='none'
     })
 })
